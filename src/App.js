a5e929ecf0521d9165e1f603fa7bb1f726248a85
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-06-29 13:50:20 +0100

    added video player component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <VideoPlayer />
+    </div>
+  );
 }
 
 export default App;
